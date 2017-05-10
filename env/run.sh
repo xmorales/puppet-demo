8e6b062c44f04f9207311efcbfe75b05a0a7197e
Xavier Morales <xavier.morales@amaris.com>
2017-05-10 12:03:56 +0200

    Adapt to jenkins execution

--- env/run.sh
+++ env/run.sh
@@ -6,5 +6,6 @@ fail() {
 [[ -d $1 ]] || fail "I need the exercise to execute"
 images="puppet-demo:centos6 puppet-demo:centos7 puppet-demo:ubuntu14.04"
 for i in $images; do
-  docker run --rm -ti -v $(realpath ${1}):/opt/puppet $i puppet apply --modulepath /opt/puppet/modules:/etc/puppet/modules /opt/puppet/apply.pp
+  echo "Proceeding with base $i..."
+  docker run --rm -v $(realpath ${1}):/opt/puppet $i puppet apply --modulepath /opt/puppet/modules:/etc/puppet/modules /opt/puppet/apply.pp
 done
