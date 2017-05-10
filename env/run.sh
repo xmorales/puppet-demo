8eac3f9c4b874bf790717f484cfe37610f856ce7
Xavier Morales <xavier.morales@amaris.com>
2017-05-10 11:35:30 +0200

    Fix volume name

--- env/run.sh
+++ env/run.sh
@@ -6,5 +6,5 @@ fail() {
 [[ -d $1 ]] || fail "I need the exercise to execute"
 images="puppet-demo:centos6 puppet-demo:centos7 puppet-demo:ubuntu14.04"
 for i in $images; do
-  docker run --rm -ti -v ${1}:/opt/puppet $i puppet apply --modulepath /opt/puppet/modules:/etc/puppet/modules /opt/puppet/apply.pp
+  docker run --rm -ti -v $(realpath ${1}):/opt/puppet $i puppet apply --modulepath /opt/puppet/modules:/etc/puppet/modules /opt/puppet/apply.pp
 done
