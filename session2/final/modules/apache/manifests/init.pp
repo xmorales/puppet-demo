27dc1062216a5017da806764a35166627214bd95
Xavier Morales <xavier.morales@amaris.com>
2017-05-24 12:12:08 +0200

    Fix some typos

--- session2/final/modules/apache/manifests/init.pp
+++ session2/final/modules/apache/manifests/init.pp
@@ -1,54 +1,54 @@
-class apache inherits apache::params {
+class apache inherits ::apache::params {
   package { 'httpd':
     ensure => present,
     name   => $::apache::params::apache_name,
     notify => Service['httpd'],
   }
-  file {"${::apache::params::httpd_dir}":
+  file { "${::apache::params::httpd_dir}":
     ensure  => directory,
-    owner   => ${::apache::params::user},
-    group   => ${::apache::params::group},
+    owner   => "${::apache::params::user}",
+    group   => "${::apache::params::group}",
     mode    => '0644',
     require => Package['httpd'],
     notify  => Service['httpd'],
   }
-  file {"${::apache::params::httpd_dir}/${::apache::params::conf_file}":
+  file { "${::apache::params::httpd_dir}/${::apache::params::conf_file}":
     ensure  => file,
-    content => template("${::apache::params::conf_template}",
-    owner   => ${::apache::params::user},
-    group   => ${::apache::params::group},
+    content => template("${::apache::params::conf_template}"),
+    owner   => "${::apache::params::user}",
+    group   => "${::apache::params::group}",
     mode    => '0644',
     require => Package['httpd'],
     notify  => Service['httpd'],
   }
   file {"${::apache::params::doc_root}":
     ensure  => directory,
-    owner   => ${::apache::params::user},
-    group   => ${::apache::params::group},
+    owner   => "${::apache::params::user}",
+    group   => "${::apache::params::group}",
     mode    => '0644',
     require => Package['httpd'],
     notify  => Service['httpd'],
   }
   file {"${::apache::params::doc_root}/index.html":
     ensure  => file,
-    source  => "puppet:///modules/apache/index.html"
-    owner   => ${::apache::params::user},
-    group   => ${::apache::params::group},
+    source  => "puppet:///modules/apache/index.html",
+    owner   => "${::apache::params::user}",
+    group   => "${::apache::params::group}",
     mode    => '0644',
     require => Package['httpd'],
     notify  => Service['httpd'],
   }
   file {"${::apache::params::doc_root}/404.html":
     ensure  => file,
-    source  => "puppet:///modules/apache/404.html"
-    owner   => ${::apache::params::user},
-    group   => ${::apache::params::group},
+    source  => "puppet:///modules/apache/404.html",
+    owner   => "${::apache::params::user}",
+    group   => "${::apache::params::group}",
     mode    => '0644',
     require => Package['httpd'],
     notify  => Service['httpd'],
   }
   service { 'httpd':
-    ensure     => started,
+    ensure     => running,
     name       => $service_name,
     enable     => true,
     hasrestart => true,
