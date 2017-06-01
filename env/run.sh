7f91f1af85b7846295d091a325b1954a82ef8ca7
Xavier Morales <xavier.morales@amaris.com>
2017-06-01 14:30:10 +0200

    Do not run on centos7 as services are not able to run

--- env/run.sh
+++ env/run.sh
@@ -5,6 +5,7 @@ fail() {
 }
 [[ -d $1 ]] || fail "I need the exercise to execute"
 images="puppet-demo:centos6 puppet-demo:centos7 puppet-demo:ubuntu14.04"
+images="puppet-demo:centos6 puppet-demo:ubuntu14.04"
 for i in $images; do
   echo "Proceeding with base $i..."
   docker run --rm --privileged -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v $(realpath ${1}):/opt/puppet $i puppet apply --modulepath /opt/puppet/modules:/etc/puppet/modules /opt/puppet/apply.pp
